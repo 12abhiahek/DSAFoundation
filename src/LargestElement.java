f073185fcef043bb67886956be2b6f136af5f93e
Abhisheksingh1812 <abhishekkumar@natlov.com>
2024-10-25 01:04:46 +0530

    secondupdate

--- src/LargestElement.java
+++ src/LargestElement.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class LargestElement {
+    public static int LargestElement(int arr[]){
+        int largerElement=arr[0];
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>largerElement){
+                largerElement=arr[i];
+            }
+
+        }
+        return largerElement;
+
+    }
     public static void main(String [] args) {
         Scanner sc=new Scanner(System.in);
         int size = sc.nextInt();
